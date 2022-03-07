ec4e89ea3c3f748bb1fac15d995ed69ada259e1b
sarah almutari <sara0moha78@gmail.com>
2022-03-07 16:32:27 +0300

    done

--- ios-cw-5/ios-cw-5/ContentView.swift
+++ ios-cw-5/ios-cw-5/ContentView.swift
@@ -9,11 +9,63 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+       
+        HStack{
+      
+        Image("download-1")
+        .clipShape(Circle())
+        .scaledToFit()
+        
+
+            Text("ios_memes")
+                .font(.body)
+                .fontWeight(.light)
+                .padding()
+        }
+        
+        
+        VStack{
+            HStack{
+                Image("image1")
+            .resizable()
+            .scaledToFit()
+.frame(width: 150.0, height: 150.0, alignment: .top)
+                Image("image2")
+                .resizable()
+                .scaledToFit()
+.frame(width: 150.0, height: 150.0, alignment: .top)
+                Image("image3")
+                    .resizable()
+                    .scaledToFit()
+.frame(width: 150.0, height: 150.0, alignment: .top)
+                
+           }
+            HStack{
+                Image("image4")
+                    .resizable()
+                    .scaledToFit()
+    .frame(width: 150.0, height: 150.0, alignment: .top)
+                Image("image5")
+                    .resizable()
+                    .scaledToFit()
+    .frame(width: 150.0, height: 150.0, alignment: .top)
+                Image("image6")
+                    .resizable()
+                    .scaledToFit()
+    .frame(width: 150.0, height: 150.0, alignment: .top)
+            }
+            
+        }
+       
+        Spacer()
+       
+           
     }
+       
 }
 
+    
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
